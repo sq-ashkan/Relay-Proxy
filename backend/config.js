1df471b5728b1e47604af471f96a25a225d9e4ad
Ashkan <ashkan.sadri.ghamshi@gmail.com>
2022-11-08 11:24:05 +0100

    all

---
+++ backend/config.js
@@ -0,0 +1,13 @@
+const PORT = 8000;
+
+// Inject this...
+const WEBSITE2 = 'http://yahoo.com';
+
+// ...into this
+const WEBSITE1 = 'http://localhost:3000/';
+
+module.exports = {
+  PORT,
+  WEBSITE1,
+  WEBSITE2,
+};
---
+++ web1/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> Hey Hallo! </p>
+        <p> hier ist ein Test</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
